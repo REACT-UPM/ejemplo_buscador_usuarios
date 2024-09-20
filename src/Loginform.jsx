e365dab16f9535b2eac07eb7d2c27381773b7dcb
Enrique <enrique.barra@upm.es>
2024-09-20 10:01:07 +0200

    login form fix

--- src/Loginform.jsx
+++ src/Loginform.jsx
@@ -5,12 +5,18 @@ export default function Loginform(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const sendForm = async (event) => {
+      //do not send form, prevent default, we send it with fetch
+      event.preventDefault();
+      props.auth(username, password);
+      setShowform(false);
+    }
 
     return (<div id="loginform">                
           {!showform ? <button className="authbutton" onClick={()=>setShowform(true)}>Iniciar sesión</button>:<form>
             <input type="text" placeholder="Usuario" value={username} onChange={e=>setUsername(e.target.value)} ></input>
             <input type="password" placeholder="Contraseña" value={password} onChange={e=>setPassword(e.target.value)}></input>
-            <button onClick={()=>props.auth(username, password)}>Enviar</button>
+            <button onClick={(e)=>sendForm(e)}>Enviar</button>
             <button onClick={()=>setShowform(false)}>Cancelar</button>
           </form>}        
       </div>)
